2792071689a462428f670b868efdc1593e3435dc
Riley M <riley_321@icloud.com>
2022-02-27 01:18:26 -0500

    Create rileys_set.js
    Special edition weapon set

---
+++ rileys_set.js
@@ -0,0 +1,5 @@
+var rileys_set = new Array(4);
+rileys_set[0] = "Black & oak wood Stiletto Switchblade";
+rileys_set[1] = "Walther Q5 Match SF Pro";
+rileys_set[2] = "Gold & oak wood AK-47";
+rileys_set[3] = "Chrome Beretta M9";
