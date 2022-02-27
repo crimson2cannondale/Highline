d49ac836aef79141176591fc017b581b6e0d4b29
Riley M <riley_321@icloud.com>
2022-02-27 01:13:44 -0500

    Create inventoryButton.js
    Code for the inventory button that I haven't added yet

---
+++ inventoryButton.js
@@ -0,0 +1,11 @@
+//Inventory button
+var invButton = document.createElement('button');
+invButton.innerText = 'inventory'
+invButton.id = 'invButton' 
+      
+invButton.addEventListener('click', () => {
+  document.write(inventory)
+})
+      
+document.body.appendChild(invButton);
+]document.write("<br>");
